b38c881912312d2648eaf3975974af9a11484a53
Grégoire Noyelle <hello@gregoirenoyelle.com>
2016-02-19 07:16:00 +0100

    Remove unused functions

--- functions.php
+++ functions.php
@@ -14,29 +14,9 @@ define( 'CHILD_THEME_VERSION', '2.1.2' );
 add_action( 'wp_enqueue_scripts', 'genesis_sample_scripts' );
 function genesis_sample_scripts() {
 
-	$minnified = '.min';
-
-	//* Should we load minified scripts? Also enqueue live reload to allow for extensionless reloading with 'grunt watch'.
-	if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG == true ) {
-
-		$minnified = '';
-		wp_enqueue_script( 'live-reload', '//localhost:35729/livereload.js', array(), CHILD_THEME_VERSION, true );
-
-	}
-
 	//* Add Google Fonts
 	wp_register_style( 'google-fonts', '//fonts.googleapis.com/css?family=Lato:300,400,700', array(), CHILD_THEME_VERSION );
 	wp_enqueue_style( 'google-fonts' );
-
-	//* Remove default CSS
-	wp_dequeue_style( 'genesis-sample-theme' );
-
-	//* Add compiled CSS
-	wp_register_style( 'genesis-sample-styles', get_stylesheet_directory_uri() . '/style' . $minnified . '.css', array(), CHILD_THEME_VERSION );
-	wp_enqueue_style( 'genesis-sample-styles' );
-
-	//* Add compiled JS
-	wp_enqueue_script( 'genesis-sample-scripts', get_stylesheet_directory_uri() . '/js/project' . $minnified . '.js', array( 'jquery' ), CHILD_THEME_VERSION, true );
 
 }
 
